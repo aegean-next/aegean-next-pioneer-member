74351ad669c30feb144c894fcf7fa82e66a127e0
RainyBlossom <bo.wu@baozuncom>
2021-02-09 17:19:22 +0800

    init

--- src/main/java/tech/aegean/next/pioneer/store/member/PioneerStoreMemberApplication.java
+++ src/main/java/tech/aegean/next/pioneer/store/member/PioneerStoreMemberApplication.java
@@ -2,9 +2,17 @@ package tech.aegean.next.pioneer.store.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class PioneerStoreMemberApplication {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PioneerStoreMemberApplication.class, args);
