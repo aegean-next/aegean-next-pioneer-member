c237f1c06a1d490ace2b281dd2088d034762c047
RainyBlossom <bo.wu@baozuncom>
2021-02-09 17:00:32 +0800

    init

---
+++ src/main/java/tech/aegean/next/pioneer/store/member/PioneerStoreMemberApplication.java
@@ -0,0 +1,13 @@
+package tech.aegean.next.pioneer.store.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PioneerStoreMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PioneerStoreMemberApplication.class, args);
+    }
+
+}
