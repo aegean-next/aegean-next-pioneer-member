461791f6083f39c4c17803491930015f8fd7a8b7
RainyBlossom <bo.wu@baozuncom>
2021-02-12 00:53:45 +0800

    update

--- src/main/java/tech/aegean/next/pioneer/store/member/PioneerStoreMemberApplication.java
+++ src/main/java/tech/aegean/next/pioneer/store/member/PioneerStoreMemberApplication.java
@@ -11,7 +11,7 @@ public class PioneerStoreMemberApplication {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello";
+        return "Hello 2021";
     }
 
     public static void main(String[] args) {
