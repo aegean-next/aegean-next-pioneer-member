3215932395f188891da97f74395465410d053715
RainyBlossom <bo.wu@baozuncom>
2021-02-12 19:36:41 +0800

    fix

--- src/main/java/tech/aegean/next/pioneer/store/member/PioneerStoreMemberApplication.java
+++ src/main/java/tech/aegean/next/pioneer/store/member/PioneerStoreMemberApplication.java
@@ -11,7 +11,7 @@ public class PioneerStoreMemberApplication {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello 2021，Happy New Year！";
+        return "Hello 2021，Happy New Years！";
     }
 
     public static void main(String[] args) {
