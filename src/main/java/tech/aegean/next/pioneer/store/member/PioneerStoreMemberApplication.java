e2d24b2196fb7e18893a747c1e8d0011cf884b64
RainyBlossom <bo.wu@baozuncom>
2021-02-19 12:59:33 +0800

    add nacos

--- src/main/java/tech/aegean/next/pioneer/store/member/PioneerStoreMemberApplication.java
+++ src/main/java/tech/aegean/next/pioneer/store/member/PioneerStoreMemberApplication.java
@@ -1,21 +1,41 @@
+/*
+ * Copyright (c) 2021. Aegean Next OpenSource FrameWork
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"): you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ *
+ * ----- ----- ----- ----- -----
+ *
+ * Visit the project homepage for more information at https://next.aegean.tech,
+ * Or see the code warehouse at https://github.com/aegean-next, https://gitee.com/aegean-next.
+ */
+
 package tech.aegean.next.pioneer.store.member;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * 启动类
+ * - Spring Boot 注解
+ * @author rainyblossom
+ */
 @SpringBootApplication
-@RestController
+@ComponentScan({"tech.aegean.next.**.controller", "tech.aegean.next.**.handler", "tech.aegean.next.**.service", "tech.aegean.next.**.config.**"})
+@MapperScan("tech.aegean.next.origin.**.mapper")
 public class PioneerStoreMemberApplication {
-
-    @RequestMapping("/")
-    public String home() {
-        return "Hello 2021，Happy New Years！";
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(PioneerStoreMemberApplication.class, args);
     }
 
 }
+
--- src/main/java/tech/aegean/next/pioneer/store/member/config/SwaggerConfig.java
+++ src/main/java/tech/aegean/next/pioneer/store/member/config/SwaggerConfig.java
@@ -1,6 +1,9 @@
 package tech.aegean.next.pioneer.store.member.config;
 
+import com.alibaba.nacos.api.config.annotation.NacosValue;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -23,19 +26,45 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("tech.aegean"))
+                .apis(RequestHandlerSelectors.basePackage(docketBasePackage))
                 .paths(PathSelectors.any())
                 .build();
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("Aegean Next Pioneer Member Service")
-                .description("Aegean Next Pioneer 会员服务")
-                .contact(new Contact("寄雨瑶花", "http://next.aegean.tech", "rainyblossom@163.com"))
-                .termsOfServiceUrl("http://pioneer-member.aegean.tech")
-                .version("0.1")
+                .title(apiInfoTitle)
+                .description(apiInfoDescription)
+                .contact(new Contact(apiInfoContactName, apiInfoContactUrl, apiInfoContactEmail))
+                .termsOfServiceUrl(apiInfoTermsOfServiceUrl)
+                .version(apiInfoVersion)
                 .build();
     }
+
+    // ----- ----- ----- -----
+
+    @NacosValue(value = "${swagger.docket.basePackage:}", autoRefreshed = true)
+    private String docketBasePackage;
+
+    @NacosValue(value = "${swagger.apiInfo.title:}", autoRefreshed = true)
+    private String apiInfoTitle;
+
+    @NacosValue(value = "${swagger.apiInfo.description:}", autoRefreshed = true)
+    private String apiInfoDescription;
+
+    @NacosValue(value = "${swagger.apiInfo.contact.name:}", autoRefreshed = true)
+    private String apiInfoContactName;
+
+    @NacosValue(value = "${swagger.apiInfo.contact.url:}", autoRefreshed = true)
+    private String apiInfoContactUrl;
+
+    @NacosValue(value = "${swagger.apiInfo.contact.email:}", autoRefreshed = true)
+    private String apiInfoContactEmail;
+
+    @NacosValue(value = "${swagger.apiInfo.termsOfServiceUrl:}", autoRefreshed = true)
+    private String apiInfoTermsOfServiceUrl;
+
+    @NacosValue(value = "${swagger.apiInfo.version:}", autoRefreshed = true)
+    private String apiInfoVersion;
 
 }
